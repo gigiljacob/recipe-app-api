bed88ff99b1f85767c8575d93ae8236f18a65cad
Gigil Jacob <gigiljacob93@gmail.com>
2021-09-19 02:37:09 +0530

    Configured postgres database

---
+++ .py_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/whitecarbon/Projects/recipe-app/recipe-app-api/.py_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
